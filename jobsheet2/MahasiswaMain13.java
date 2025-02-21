7d19e5a66bac3b400755926fac1de41dabfe6445
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2025-02-21 10:56:52 +0700

    Update MahasiswaMain13.java

--- jobsheet2/MahasiswaMain13.java
+++ jobsheet2/MahasiswaMain13.java
@@ -6,11 +6,19 @@ public class MahasiswaMain13 {
     mhs1.nama = "Khesa Kresna Adjiengrat";
     mhs1.nim  = "244107020233";
     mhs1.kelas = "TI-1B";
-    mhs1.ipk= 2.92;
+    mhs1.ipk=3.12;
 
     mhs1.tampilkanInformasi();
     mhs1.ubahKelas("TI-2B");
     mhs1.updateIpk(4.00);
     mhs1.tampilkanInformasi();
+
+    mahasiswa13 mhs2 = new mahasiswa13("Projekt red","2141720160",3.45, "TI-2A");
+    mhs2.updateIpk(3.30);
+    mhs2.tampilkanInformasi();
+
+    mahasiswa13 mhs3 = new mahasiswa13("Huo-Huo","217463728",3.12, "TI-2d");
+    mhs2.updateIpk(3.59);
+    mhs2.tampilkanInformasi();
 }
 }
